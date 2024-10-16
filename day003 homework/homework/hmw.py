92f67be8e9741e7467e847efc159a6d98792c5f3
jeko115 <jekoqurdovanidze11@gmail.com>
2024-10-17 00:06:34 +0400

    first commit

---
+++ day003 homework/homework/hmw.py
@@ -0,0 +1,46 @@
+# სამი ინტეჯერ ცვლადის დეკლარაცია
+
+a = 5
+b = 10
+c = 15
+
+# მათი ჯამი და დაპრინტვა
+
+sum_of_numbers = a + b + c
+print (sum_of_numbers)
+
+# სტრინგ ცვლადის დეკლარაცია 
+
+name = "ჯეკო"
+surname = "ქურდოვანიძე"
+
+# დაბეჭვდა
+
+print (name, surname)
+
+# float ცვლადის დეკლარაცია
+height = 1.81
+
+print (height)
+
+x = 15
+x = x + 5
+
+print (x)
+
+# სტრინგ ცვლადების დეკლარაცია
+
+name = "ჯეკო"
+surname = "ქურდოვანიძე"
+
+#კონკატენაცია
+full_name = name + " " + surname
+
+print (full_name)
+
+# ინტეჯერის ტიპის ცვლადის დეკლარაცია
+price = 100
+
+result = price * 5
+
+print (result)
