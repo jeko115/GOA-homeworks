6aeb36e6e6676d79b463041d65bb4886480a1995
jeko115 <jekoqurdovanidze11@gmail.com>
2024-10-27 01:05:04 +0400

    first commit

---
+++ day01/homework/homwork.py
@@ -0,0 +1,32 @@
+name= "jeko"
+#name არის ცვლადი
+#= არის ცვლადისთვის მიმნიჭებელი სიმბოლო
+"jeko" #არის ცვლადისთვის მნიშვნელობ
+surname= "qurdovanidze"
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name= "jeko" # ეს არის str(string) ტიპის ცვლადი
+age = 19 #ეს არის int( integer ) მთელი რიცხვი
+height = 181.5 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = True #true or false
+is_ugly = False #snakecase( უბრალოდ წერის სტილი სტანდარტულად )
+
+is_ugly =False # ჯავასკრიპტული camelcase
+
+
+#print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები 
+##print(name + age)
+
+print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + " " + str(age))
---
+++ day04/homework/homework.py
@@ -0,0 +1,27 @@
+a = 5
+b = 10
+c = 15
+
+ჯამი = a + b + c
+print("ჯამი არის:", ჯამი)
+
+name = "ჯეკო"
+print(name)
+
+height = 1.81
+print(height)
+
+x = 15
+x += 5
+print(x)
+
+name ="ჯეკო"
+surname ="ქურდოვანიძე"
+სრული_სახელი = name + " " + surname
+print(სრული_სახელი)
+
+price = 100
+result = price * 5
+print(result)
+
+
