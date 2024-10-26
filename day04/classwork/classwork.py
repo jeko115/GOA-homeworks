21498004b17ef69b942ac4f9b3320b0e41a2adf9
jeko115 <jekoqurdovanidze11@gmail.com>
2024-10-27 03:18:07 +0400

    second commit

--- day04/classwork/classwork.py
+++ day04/classwork/classwork.py
@@ -1,1 +1,22 @@
-# noclasswork
+name = "ჯეკო"   # სახელი
+height = 182      # სიმაღლე სანტიმეტრებში
+age = 19         # ასაკი
+
+# ცვლადების ბეჭდვა
+print("Name:", name)
+print("Height:", height)
+print("Age:", age)
+
+
+num1 = 10
+num2 = 3
+
+ num1 + num2
+num1 - num2
+num1 * num2
+ num1 / num2 
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
