af00acc211aa67071bfd6686006716766023302c
jeko115 <jekoqurdovanidze11@gmail.com>
2024-11-11 00:50:11 +0400

    third commit

---
+++ day10/homework/homework.py
@@ -0,0 +1,20 @@
+print("გამარჯობა,", input("ჯეკო: ") + "!")
+
+num = int(input("5: "))
+
+# რიცხვისა და მისი ნამრავლის 10-ზე დაბეჭდვა
+print("5:", num)
+print("რიცხვის ნამრავლი 10-ზე:", num * 10)
+
+num1 = int(input("10: "))
+num2 = int(input("5: "))
+
+# რიცხვის სხვაობის გამოთვლა და დაბეჭდვა
+difference = num1 - num2
+print("5:", difference)
+
+first_name = input("ჯეკო: ")
+last_name = input("ქურდოვანიძე: ")
+
+# დაბეჭდვა
+print("თქვენი სახელია", first_name, last_name, "!")
