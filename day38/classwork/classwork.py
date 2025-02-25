2ee88e10230fe52e0b1d9b2d57e9146999bbadbf
jeko115 <jekoqurdovanidze11@gmail.com>
2025-02-25 23:43:31 +0400

    thirtyninth

---
+++ day38/classwork/classwork.py
@@ -0,0 +1,12 @@
+def greet():
+    return "hello world!"
+
+print(greet())  # Output: "hello world!"
+
+
+
+def multiply(a, b):
+    return a * b  # Returns the product
+
+# Example usage:
+print(multiply(3, 4))  # Output: 12
---
+++ day38/homework/homwrok.py
@@ -0,0 +1,28 @@
+def number_to_string(n):
+    return str(n)
+
+# Examples
+print(number_to_string(123))   # "123"
+print(number_to_string(999))   # "999"
+print(number_to_string(-100))  # "-100"
+
+
+
+
+def repeat_string(n, s):
+    return s * n
+
+# Examples
+print(repeat_string(6, "I"))      # "IIIIII"
+print(repeat_string(5, "Hello"))  # "HelloHelloHelloHelloHello"
+
+
+
+def string_to_number(s):
+    return int(s)
+
+# Examples
+print(string_to_number("1234"))  # 1234
+print(string_to_number("605"))   # 605
+print(string_to_number("1405"))  # 1405
+print(string_to_number("-7"))    # -7
